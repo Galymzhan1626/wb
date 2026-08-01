1ce2bbaa0fd02292c5805a5cfddba33c60b6a14d
Galymzhan1626 <154108707+Galymzhan1626@users.noreply.github.com>
2026-08-01 23:28:13 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
+import streamlit_authenticator as stauth
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
